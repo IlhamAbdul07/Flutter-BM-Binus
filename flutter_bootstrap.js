2adb54f85a4afd03cb073bd5a0be927242e63e41
IlhamAbdul07 <IlhamAbdul07@users.noreply.github.com>
2025-10-31 13:35:21 +0000

    deploy: 7a4cb7ccac660c67ed4dc09b4680831865bee21e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4027210835"
+    serviceWorkerVersion: "4218715765"
   }
 });
